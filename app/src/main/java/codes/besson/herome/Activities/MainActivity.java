0879f2922bd0dea3e06b5dac2299b0dcaa6c4ed0
Alex-Besson <alex_j67@hotmail.com>
2016-11-12 17:08:09 -0500

    Connected fragment to main activity. Got rid of action bar.

--- app/src/main/java/codes/besson/herome/Activities/MainActivity.java
+++ app/src/main/java/codes/besson/herome/Activities/MainActivity.java
@@ -1,15 +1,32 @@
 package codes.besson.herome.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
+import codes.besson.herome.Fragments.MainFragment;
 import codes.besson.herome.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            manager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+
+    @Override
+    public void onMainFragmentInteraction(Uri uri) {
+
     }
 }
